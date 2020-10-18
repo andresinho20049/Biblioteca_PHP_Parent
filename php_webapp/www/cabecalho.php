93d45b8b773f4b198b2f8e28245063037104a9e9
Andre Carlos <andre.andresinho2009@hotmail.com>
2020-10-18 14:45:40 -0300

    Incluindo rodape e cabecalho

---
+++ php_webapp/www/cabecalho.php
@@ -0,0 +1,59 @@
+<?php
+    
+    $paginas = array('home'=>'home.php',
+                'produtos'=>'produtos.php',
+                'artigos'=>array('novo-artigo'=>'novoArquivo.php','consulta'=>'consulta.php'),
+                'sobre'=>'sobre.php');
+?>
+<body>
+    <header>
+        <img src="./img/cabecalho.jpg" width=100% alt="" />
+        <nav id="navbar_top" class="navbar navbar-expand-md bg-dark navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="home">
+                    <img src="./img/Logo.png" width="45" height="35" class="d-inline-block align-center" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <?php
+                            foreach ($paginas as $key=>$value){
+                                #Verificar se não é array
+                                if(!is_array($value)){
+                                    #Links
+                                    echo '
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="?page='.$key.'">
+                                            '.ucfirst($key).'
+                                        </a>
+                                    </li>
+                                    ';
+                                } else{
+                                    #Dropdown
+                                    echo '<li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">'.ucfirst($key).'</a>
+                                        <div class="dropdown-menu" aria-labelledby="navbardrop">';
+                                            foreach($value as $key_2=>$value_2){
+                                                echo '<a class="dropdown-item" href="?page='.$key.'&subpage='.$key_2.'">'.ucfirst($key_2).'</a>';
+                                            }
+                                        echo '</div></li>';
+                                }
+                            }
+                        ?>
+                    </ul>
+                    <div class="d-none d-md-block">
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
